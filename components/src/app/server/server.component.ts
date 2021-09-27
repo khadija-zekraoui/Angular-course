a97ef4c392c48a79e98993d3f571007e4f77a2ea
khadija-zekraoui <h.zekraoui.khadija@gmail.com>
2021-09-27 20:36:23 +0200

    Creating a new component

---
+++ components/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server', // Is the string used to insert the component
+    templateUrl: './server.component.html' // The HTML template
+})
+//same name of the file
+export class ServerComponent {
+
+}
