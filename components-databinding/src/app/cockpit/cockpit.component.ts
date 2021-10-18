72f802c8d4af2c0e8a443f30eb9f4031d7b1ef9b
khadija-zekraoui <h.zekraoui.khadija@gmail.com>
2021-10-18 21:10:23 +0200

    Local references --> ViewChild()

--- components-databinding/src/app/cockpit/cockpit.component.ts
+++ components-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Server } from '../models/server.model';
 
 
@@ -11,21 +11,30 @@ export class CockpitComponent implements OnInit {
   // Decorator to make a event listenable form outside
   @Output() serverCreated = new EventEmitter<Server>();
   @Output() blueprintCreated = new EventEmitter<Server>();
-  newServerName = '';
-  newServerContent = '';
+  
+  // Select component by reference which it allows getting direct access to elements in the DOM
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(serverNameInput: HTMLInputElement) {
     // To emit the custom event
-    this.serverCreated.emit(new Server('server', this.newServerName, this.newServerContent));
+    this.serverCreated.emit(
+      new Server(
+        'server',
+         serverNameInput.value, // this is an HTML element
+         this.serverContentInput.nativeElement.value)); // this is an element ref 
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
     // To emit the custom event
-    this.blueprintCreated.emit(new Server('blueprint', this.newServerName, this.newServerContent));
+    this.blueprintCreated.emit(
+      new Server(
+        'blueprint',
+        serverNameInput.value,
+        this.serverContentInput.nativeElement.value));
   }
 
 }
