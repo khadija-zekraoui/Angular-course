27c8da628557b181acb5125961525befbf613aec
khadija-zekraoui <h.zekraoui.khadija@gmail.com>
2021-11-27 21:01:37 +0100

    Routing: Using Relative Paths in Programmatic Navigation

--- routing/src/app/components/servers/servers.component.ts
+++ routing/src/app/components/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,22 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  // Inject  ActivatedRoute => gives the currently active page
+  constructor(private serversService: ServersService, 
+              private router: Router, 
+              private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // this.router.navigate(['servers']); // relative path Incorrect to reload servers page, but doesn't cause any errors because it doesn't know the currently active route
+
+    // To use relative routing it's necessary to add a configuration to set the current page
+    // this.router.navigate(['servers'], {relativeTo: this.route}); // Triggers an error because localhost:4200/servers/servers doesn't exist as a route in the configuration routing
+    
+    this.router.navigate(['/servers']); // absolute path Works !!!
   }
 
 }
