c805a8f64bae4f94d074601bef3b8b05dfd8c352
khadija-zekraoui <h.zekraoui.khadija@gmail.com>
2021-11-28 14:53:46 +0100

    Routing: Using Query Parameters

--- routing/src/app/components/servers/server/server.component.ts
+++ routing/src/app/components/servers/server/server.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(+this.route.snapshot.params['id']); // + to parse the id from string to number
+    this.server = this.serversService.getServer(
+      +this.route.snapshot.params['id']
+    ); // + to parse the id from string to number
     this.route.params.subscribe((params: Params) => {
-      this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
+      this.server = this.serversService.getServer(
+        +this.route.snapshot.params['id']
+      );
     });
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], 
+    {
+      relativeTo: this.route, // Active page is http://localhost:4200/servers/1
+    });
+  }
 }
