5080df733623ec69a92a3b25149fbc7a26049a08
khadija-zekraoui <h.zekraoui.khadija@gmail.com>
2021-12-12 16:43:43 +0100

    Reactive Forms: Grouping Controls

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -4,10 +4,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   genders = ['male', 'female'];
   // In Angular a form is a a group of controls and what is FormGroup holds
   signupForm!: FormGroup;
@@ -15,15 +14,21 @@ export class AppComponent implements OnInit {
   // Create a form and connect it to the HTML code
   // Initialize the form before rendering the template
   // With Validators.required we only pass the reference to this method and Angular will execute it whenever it detects changes in this input
+  // we can use FormGroup inside another FormGroup to create a nested form
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('female'),
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl('female'),
     });
   }
 
   onSubmit() {
-    console.log("file: app.component.ts ~ line 26 ~ AppComponent ~ onSubmit ~ this.signupForm", this.signupForm)
+    console.log(
+      'file: app.component.ts ~ line 26 ~ AppComponent ~ onSubmit ~ this.signupForm',
+      this.signupForm
+    );
   }
 }
