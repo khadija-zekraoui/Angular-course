04827a0ffbdc0ccc5a04fd1645f5edba7c0ac11d
khadija-zekraoui <h.zekraoui.khadija@gmail.com>
2021-12-18 18:18:02 +0100

    Reactive Form: Creating a Custom Async Validator

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -7,6 +7,7 @@ import {
   ValidatorFn,
   Validators,
 } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -27,8 +28,15 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails.bind(this)
+        ),
       }),
       gender: new FormControl('female'),
       hobbies: new FormArray([]),
@@ -42,9 +50,7 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     // bind this because Angular will call this.forbiddenNames()
     (<FormArray>this.signupForm.get('hobbies')).push(
-      new FormControl(null, [
-        Validators.required,
-      ])
+      new FormControl(null, [Validators.required])
     );
   }
 
@@ -60,10 +66,25 @@ export class AppComponent implements OnInit {
   // Is a custom validator
   // It needs to receive an argument which is the control it should check
   // It has to return an object respecting a specific syntax {key: boolean}
-  forbiddenNames(control: FormControl): {[key: string]: boolean} {
-    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {  // -1 means true
-      return {'nameIsForbidden': true};
+  forbiddenNames(control: FormControl): { [key: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      // -1 means true
+      return { nameIsForbidden: true };
     }
-    return null;  // this tells angular the condition is valid
+    return null; // this tells angular the condition is valid
+  }
+
+  // Asynchronous validator
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value.includes('test')) {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
